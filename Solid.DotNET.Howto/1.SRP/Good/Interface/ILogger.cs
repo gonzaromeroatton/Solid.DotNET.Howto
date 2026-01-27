007002e59ea83b805731a1a222391c4afc39bf49
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2026-01-27 12:52:01 -0300

    Implementación SRP: ejemplo bueno y malo en InvoiceService
    Se crea la solución y proyecto base para .NET 9.0. Se añade una versión que viola el SRP y otra que lo respeta en la clase InvoiceService, junto con las interfaces IInvoiceRepository y ILogger para separar responsabilidades. Esto ilustra la diferencia entre una mala y una buena aplicación del Principio de Responsabilidad Única.

---
+++ Solid.DotNET.Howto/1.SRP/Good/Interface/ILogger.cs
@@ -0,0 +1,13 @@
+﻿namespace Solid.DotNET.Howto._1.SRP.Good.Interface;
+
+/// <summary>
+/// 
+/// </summary>
+public interface ILogger
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    void Log(string message);
+}
