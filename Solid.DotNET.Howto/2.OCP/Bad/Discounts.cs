1b23b5b3109c250378c0b292a5bcde2ea9717695
Gonza Romero Atton <148910080+gonzaromeroatton@users.noreply.github.com>
2026-01-27 16:32:54 -0300

    Implementación de OCP con ejemplos bueno y malo
    Se añaden dos versiones de la clase Discounts: una que viola el principio OCP usando switch por tipo de cliente, y otra que lo respeta mediante la interfaz IDiscount. Se incluye la interfaz IDiscount para facilitar la extensión de tipos de descuento sin modificar el código existente.

---
+++ Solid.DotNET.Howto/2.OCP/Bad/Discounts.cs
@@ -0,0 +1,24 @@
+﻿namespace Solid.DotNET.Howto._2.OCP.Bad;
+
+/// <summary>
+/// 
+/// </summary>
+public sealed class Discounts
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="amount"></param>
+    /// <returns></returns>
+    public decimal CalculateDiscount(string type, decimal amount)
+    {
+        return type switch
+        {
+            "Regular" => amount * 0.05m,
+            "Premium" => amount * 0.10m,
+            "VIP" => amount * 0.15m,
+            _ => 0m,
+        };
+    }
+}
---
+++ Solid.DotNET.Howto/2.OCP/Good/Interface/IDiscount.cs
@@ -0,0 +1,10 @@
+﻿namespace Solid.DotNET.Howto._2.OCP.Good.Interface;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IDiscount
+{
+    bool IsMatch(string type);
+    decimal CalculateDiscount(decimal amount);
+}
